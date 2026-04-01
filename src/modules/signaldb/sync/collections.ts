1ead09c7e08f06dd42c4b5a64015a708d575c2f0
meerkapp <dev@mee.li>
2026-04-02 01:42:01 +0300

    fix(signaldb): add explicit CollectionRegistryEntry interface for collectionsRegistry

--- src/modules/signaldb/sync/collections.ts
+++ src/modules/signaldb/sync/collections.ts
@@ -1,6 +1,18 @@
+import type { Collection } from '@signaldb/core'
 import Localities from '../models/localities.model'
 import Countries from '../models/countries.model'
 import Organizations from '../models/organizations.model'
 import Warehouses from '../models/warehouses.model'
 
-export const collectionsRegistry = [Countries, Localities, Organizations, Warehouses]
+export interface CollectionRegistryEntry {
+  collection: Collection<any>
+  collectionName: string
+  tableName: string
+}
+
+export const collectionsRegistry: CollectionRegistryEntry[] = [
+  Countries,
+  Localities,
+  Organizations,
+  Warehouses,
+]
